97feef03e9807eb115b62a863464c569fa40790c
Paddy Hayes <Paddy.Hayes@students.ittralee.ie>
2019-09-12 12:53:35 +0100

    Initial commit

---
+++ src/Set02.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Set02 {
+    public static void main(String args[]){
+        String FirstName, Initial, LastName;
+        int Distance;
+        float LessThanTen = 0.07f, GreaterThanTen = 0.1f;
+
+        Scanner input=new Scanner(System.in);
+
+        System.out.print("Please enter your first name: ");
+        FirstName = input.nextLine();
+
+        System.out.print("Please enter your last name: ");
+        LastName = input.nextLine();
+
+        System.out.print("Please enter your initials: ");
+        Initial = input.nextLine();
+
+        System.out.print("Please enter distance traveled: ");
+        Distance = input.nextInt();
+
+        if()
+
+    }
+}
